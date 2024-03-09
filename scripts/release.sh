74938703a5da176874cdb8f72ef53c84bd29b130
kazuya kawaguchi <kawakazu80@gmail.com>
2024-03-09 23:39:49 +0900

    more setup

---
+++ scripts/release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# Restore all git changes
+git restore --source=HEAD --staged --worktree -- package.json pnpm-lock.yaml
+
+# Update token
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release packages
+for PKG in packages/* ; do
+  if [[ -d $PKG ]]; then
+    pushd $PKG
+    TAG="latest"
+    echo "⚡ Publishing $PKG with tag $TAG"
+    pnpm publish --access public --no-git-checks --tag $TAG
+    popd > /dev/null
+  fi
+done
