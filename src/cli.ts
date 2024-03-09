ef488595326585e4925669fcfd2e944cb92c9034
kazuya kawaguchi <kawakazu80@gmail.com>
2024-03-10 00:21:13 +0900

    fix: setup cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,37 @@
-console.log('hello rolldown!')
+import { readFileSync } from 'node:fs'
+import { defineCommand, runMain } from 'citty'
+import { consola } from 'consola'
+import path from 'node:path'
+import { URL } from 'node:url'
+import { definePackageJSON } from 'pkg-types'
+
+function getPackageJSON(base: string) {
+  try {
+    const rawData = readFileSync(path.join(base, '../package.json'), 'utf8')
+    return definePackageJSON(JSON.parse(rawData))
+  } catch (error) {
+    return { version: '0.0.0' }
+  }
+}
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const { name: rawName, version, description } = getPackageJSON(__dirname)
+const name = rawName?.split('/').pop()
+consola.debug('name', name)
+consola.debug('version', version)
+
+const main = defineCommand({
+  meta: {
+    name,
+    version,
+    description
+  },
+  args: {
+    // TODO:
+  },
+  async run({ args }) {
+    consola.debug('args', args)
+  }
+})
+
+runMain(main)
